75d3366b3643d388f1245daa35672d271822a859
Strange <StrangeGuy6228@protonmail.com>
2024-01-02 22:22:45 +0530

    Set up chat.php and embeddd chat_iframe.php in it.

--- components/navbar.php
+++ components/navbar.php
@@ -1,4 +1,13 @@
 <?php
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    header("Location: /home.php");
+    exit();
+}
+
 $root = $_SERVER['DOCUMENT_ROOT'];
 
 echo "
@@ -14,13 +23,12 @@ echo "
         <a class='nav-link' href='/home.php'>Home <span class='sr-only'>(current)</span></a>
       </li>";
 
-if ( session_status() == PHP_SESSION_NONE ) {
+if (!isset($_SESSION['userName'])) {
     echo "
       <li class='nav-item'>
         <a class='nav-link' href='/login_system/signup.php'>Sign Up</a>
     </li>";
-}
-elseif ( session_status() == PHP_SESSION_ACTIVE ){
+} elseif (isset($_SESSION['userName'])) {
     echo "
       <li class='nav-item'>
         <a class='nav-link' href='/login_system/logout.php'>Log out</a>
--- login_system/logout.php
+++ login_system/logout.php
@@ -1,10 +1,20 @@
 <?php
-    header("Cache-Control: no-store, no-cache, must-revalidate");
-    session_start();
-    $root = $_SERVER['DOCUMENT_ROOT'];
-    require $root . "/components/navbar.php";
-    $_SESSION = array();
-    session_destroy();
-    header("Location: /home.php");
-    exit();
+
+header("Cache-Control: no-store, no-cache, must-revalidate");
+session_start();
+
+$root = $_SERVER['DOCUMENT_ROOT'];
+require_once($root . "/components/navbar.php");
+require_once($root . "/login_system/dbconnect.php");
+
+$username = mysqli_real_escape_string($conn, $_SESSION['userName']);
+$sql = "DELETE FROM users_logged_in WHERE users_logged_in.username = '$username'";
+$result = mysqli_query($conn, $sql);
+
+$_SESSION = array();
+session_destroy();
+
+header("Location: /home.php");
+exit();
+
 ?>
