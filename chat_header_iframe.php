ef24a6c6fd39a0f42f98e9ddd5e0b4d9863e5f82
Strange <StrangeGuy6228@protonmail.com>
2024-01-08 00:21:07 +0530

    update

--- chat_header_iframe.php
+++ chat_header_iframe.php
@@ -1,7 +1,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/css_reset.css" class="css">
     <title>Home Page</title>
 </head>
--- chat_message_iframe.php
+++ chat_message_iframe.php
@@ -4,28 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
     <meta http-equiv="refresh" content="10"> <!-- Refresh every 5 seconds -->
     <link rel="stylesheet" href="/css/css_reset.css" class="css">
-    <link rel="stylesheet" href="/css/chat_iframe.css" class="css">
+    <link rel="stylesheet" href="/css/chat_message_iframe.css" class="css">
     <title>chat_iframe</title>
 </head>
 
-<style>
-    .user_messages {
-        margin: 0;
-        padding: 5px;
-    }
-
-    .message-info {
-        font-weight: bold;
-        margin-right: 10px;
-    }
-
-    .message-text {
-        margin-left: 10px;
-    }
-</style>
 
 
 <body>
